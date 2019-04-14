e627c47062748aa9531bf69305cd0ecd3bcb3e87
unknown <vasileios.m.tsakalos@outlook.com>
2019-04-14 21:48:49 +0200

    Add grammar

---
+++ B2/Grammatik/ZweiteilligeKonnektoren.py
@@ -0,0 +1,13 @@
+Konnektoren = {
+"Definition": "Zweiteillige Konnektoren haben verschiedene Funktionen: Aufzählungen, Alternatienm Geensätzen und Einschränkungen. Sie können auf vershiedenen Positionen stehen",
+"Aufzählung positiv": ["Lange haben wir uns nicht nur regelmaßig geschrieben, sodern (wir haben) auch oft telefoniert",
+"Wir haben sowohl in derselben Firma gearbeitet als auch im selben Chor gesungen"],
+"Aufzählung negativ": ["Es macht weder meinem Freund noch mir etwas aus.",
+"Weder meinem Freund noch mir macht es etwas aus."],
+"Alternativ": ["Entweder gehen wir etwas essen oder (wir) treffen uns zu Hause.",
+"Wir gehen entweder etwas essen oder (wir) treffen uns zu Hause."],
+"Gegensatz": ["Einerseits würde ich ihn gern treffen, andererseits bringt das nichts.",
+"Ich würde ihn einerseits gern treffen, andererseits bringt das nicht"],
+"Einschränkung": ["Wir sehen uns zwar nicht mehr oft, aber wir bleiben Freunde",
+"Zwar sehen wir uns nicht mehr oft, aber wir bleiben Freunde"]
+}
