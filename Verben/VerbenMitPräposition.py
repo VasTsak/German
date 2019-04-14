b1d818931d827c3aacca8dd620fa61fc3ceec8ef
unknown <vasileios.m.tsakalos@outlook.com>
2019-04-14 20:55:31 +0200

    Start the file with verbs with prepositions

---
+++ Verben/VerbenMitPräposition.py
@@ -0,0 +1,10 @@
+dictionary = {
+"abhangen von":["depend on", "Ottos Stimmung hängt stark vom Wetter ab."],
+"ableiten aus": ["derive from", "Man kann das Wort 'Fenster' aus dem Lateinischen ableiten."],
+"absehen von": ["refrain from", "Wenn man vom schlechten Wetter absieht, war unser Urlau schön."],
+"achten auf": ["watch", "Der Busfahrer hatte nicht auf den Gegenverkehr geachtet."],
+"anfangen mit": ["start with", "Jedes geschriebene deutsche Nomen fängt mit einem Großbuchstaben an."],
+"antworten auf": ["answer to", "Ich habe sofort auf seinen Brief geantwortet."],
+"arbeiten an": ["working on", "Die Autorin arbeitet an einem neuen Roman."],
+"sich ärgern über": ["to be agry with", "Ich ärgere mih über meine Fehler."]
+}
